d1d2924460c2c06f0539108fdc10590a7b1ac3c8
Rhys Howell <howell33@purdue.edu>
2014-09-20 02:40:07 -0400

    Basic jade page up, adding some functionality

--- server.js
+++ server.js
@@ -2,18 +2,45 @@ var express = require('express');
 var app = express();
 var port = 3000;
 var userCounter = 0;
+var path = require('path');
 
-//render template
-app.engine('jade', require('jade').__express);
-app.set('views', './views')
+var routes = require('./routes/index');
+
+app.listen(port);
+
+// render engine setup
+app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
-//install server
-var server = app.listen(port, function() {
-    console.log('Listening on port %d', server.address().port);
+app.use('/', routes);
+
+// 404 forward to error handler
+app.use(function(req, res, next) {
+    var err = new Error('Not Found');
+    err.status = 404;
+    next(err);
 });
 
-app.get('/', function (req, res) {
-    userCounter++;
-    res.render('index', { title: 'Easy App', message: 'Hello there user #'+userCounter+'!'});
-})
+// error handlers
+
+// development error handler
+// will print stacktrace
+if (app.get('env') === 'development') {
+    app.use(function(err, req, res, next) {
+        res.status(err.status || 500);
+        res.render('error', {
+            message: err.message,
+            error: err
+        });
+    });
+}
+
+// production error handler
+// no stacktraces leaked to user
+app.use(function(err, req, res, next) {
+    res.status(err.status || 500);
+    res.render('error', {
+        message: err.message,
+        error: {}
+    });
+});
