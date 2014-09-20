92ee7549fec36275c75df154437105a39d22df48
Ben Alderfer <balderfe@purdue.edu>
2014-09-20 13:25:38 -0400

    magic

--- routes/index.js
+++ routes/index.js
@@ -6,7 +6,17 @@ var requestCounter = 0;
 /* GET home page. */
 router.get('/', function(req, res) {
     requestCounter++;
-    res.render('index', { title: 'Easy App' , message: 'Welcome to Easy App total requests: '+ requestCounter + '.'});
+    res.render('index', { title: 'Shotgun' , message: 'Welcome to Easy App total requests: '+ requestCounter + '.'});
+});
+
+router.get('/home', function(req, res) {
+    requestCounter++;
+    res.render('home', { title: 'Select a Type' , message: 'Welcome to Easy App total requests: '+ requestCounter + '.'});
+});
+
+router.get('/hackathon', function(req, res) {
+    requestCounter++;
+    res.render('hackathon', { title: 'Hackathon' , message: 'Welcome to Easy App total requests: '+ requestCounter + '.'});
 });
 
 module.exports = router;
