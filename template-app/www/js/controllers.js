47e0f2a211128e7fdd63124af118b078b1677196
Kirby Kohlmorgen <kirby@purdue.edu>
2014-09-20 05:11:04 -0400

    Forgot controllers.js

--- template-app/www/js/controllers.js
+++ template-app/www/js/controllers.js
@@ -1,20 +1,17 @@
 angular.module('starter.controllers', [])
 
-.controller('UpdatesCtrl', function($scope, Updates) {
-  $scope.updates = Updates.all();
+.controller('UpdatesCtrl', function($scope, $firebase) {
+  var ref = new Firebase("https://easy-app.firebaseio.com/updates");
+  var sync = $firebase(ref);
+  var syncObject = sync.$asObject();
+  syncObject.$bindTo($scope, "updates");
 })
-
-// .controller('MentorsCtrl', function($scope, Mentors) {
-//   $scope.mentors = Mentors.all();
-// })
 
 .controller('MentorsCtrl', function($scope, $firebase) {
   var ref = new Firebase("https://easy-app.firebaseio.com/mentors");
   var sync = $firebase(ref);
   var syncObject = sync.$asObject();
   syncObject.$bindTo($scope, "mentors");
-
-  // $scope.mentors = Mentors.all();
 })
 
 .controller('MentorDetailCtrl', function($scope, $stateParams, Mentors) {
