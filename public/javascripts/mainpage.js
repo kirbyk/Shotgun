9e72202a253f4342e71a98c1417c33ad06ee09f2
Rhys Howell <howell33@purdue.edu>
2014-09-20 18:26:05 -0400

    Fixed transitions and added new firebase paths

--- public/javascripts/mainpage.js
+++ public/javascripts/mainpage.js
@@ -1,28 +1,42 @@
+var activeView = 0;
+var views = [
+    '#mainInput',
+    '#updatesInput',
+    '#mentorsInput',
+    '#prizesInput',
+    '#scheduleInput'
+];
+
 $(document).ready(function() {
 
 
     $('#main').click(function(event) {
-        $('#updatesInput, #mentorsInput, #prizesInput, #scheduleInput').fadeOut(250, function() {
+        $(views[activeView]).fadeOut(250, function() {//'#updatesInput, #mentorsInput, #prizesInput, #scheduleInput'
+            activeView = 0;
             $('#mainInput').fadeIn(250);
         });
     });
     $('#updates').click(function(event) {
-        $('#mainInput, #mentorsInput, #prizesInput, #scheduleInput').fadeOut(250, function() {
+        $(views[activeView]).fadeOut(250, function() { //'#mainInput, #mentorsInput, #prizesInput, #scheduleInput'
+            activeView = 1;
             $('#updatesInput').fadeIn(250);
         });
     });
     $('#mentors').click(function(event) {
-        $('#updatesInput, #mainInput, #prizesInput, #scheduleInput').fadeOut(250, function() {
+        $(views[activeView]).fadeOut(250, function() {//'#updatesInput, #mainInput, #prizesInput, #scheduleInput'
+            activeView = 2;
             $('#mentorsInput').fadeIn(250);
         });
     });
     $('#prizes').click(function(event) {
-        $('#updatesInput, #mentorsInput, #mainInput, #scheduleInput').fadeOut(250, function() {
+        $(views[activeView]).fadeOut(250, function() {//'#updatesInput, #mentorsInput, #mainInput, #scheduleInput'
+            activeView = 3;
             $('#prizesInput').fadeIn(250);
         });
     });
     $('#schedule').click(function(event) {
-        $('#updatesInput, #mentorsInput, #prizesInput, #mainInput').fadeOut(250, function() {
+        $(views[activeView]).fadeOut(250, function() {//'#updatesInput, #mentorsInput, #prizesInput, #mainInput'
+            activeView = 4;
             $('#scheduleInput').fadeIn(250);
         });
     });
@@ -39,25 +53,28 @@ $(document).ready(function() {
         var name = $('#mentorNameInput').val();
         var skills = $('#mentorDescriptionInput').val();
 
-        //add the new mentor
-        mentorRef.child(company).push({name: name, skills: skills});
-        $('#mentorNameInput').val('');
-        $('#mentorCompanyInput').val('');
-        $('#mentorDescriptionInput').val('');
+        //add the new mentorif fields not empty
+        if(company != '' && name != '' && skills != '') {
+            mentorRef.child(company).push({name: name, skills: skills});
+            $('#mentorNameInput').val('');
+            $('#mentorCompanyInput').val('');
+            $('#mentorDescriptionInput').val('');
+        }
+        else
+            alert("Please fill in all of the fields.");
     });
     mentorRef.on('child_added', function(snapshot) {
         //response recieved from server
         var message = snapshot.val();
-        $("#mentorUpdate").html("Successful!");
-        setTimeout(function(){$("#mentorUpdate").html("");},3000)
+        successfulUpdate();
     });
 
     //update additions
     var updateRef = myDataRef.child("updates")
     $('#addUpdate').click(function() {
         //recieve new values
-        var header = $('#updateHeader').val();
-        var subheader = $('#updateSubHeader').val();
+        var header = $('#updateNameInput').val();
+        var subheader = $('#updateDescInput').val();
 
         var currentDate = new Date();
         var dateString = (currentDate.getHours()%12.0) + ":" + currentDate.getMinutes();
@@ -66,16 +83,71 @@ $(document).ready(function() {
         else
             dateString += " am";
 
-        //add the new mentor
-        updateRef.push({createdAt: dateString, header: header, subheader: subheader});
-        $('#updateHeaderInput').val('');
-        $('#updateSubHeaderInput').val('');
+        //add the new mentor if fields not empty
+        if(header != '' && subheader != '') {
+            updateRef.push({createdAt: dateString, header: header, subheader: subheader});
+            $('#updateNameInput').val('');
+            $('#updateDescInput').val('');
+        }
+        else
+            alert("Please fill in all of the fields.");
     });
     updateRef.on('child_added', function(snapshot) {
         //response recieved from server
         var message = snapshot.val();
-        $("#updateUpdate").html("Successful!");
-        setTimeout(function(){$("#updateUpdate").html("");},3000)
+        successfulUpdate();
+    });
+
+    //prize additions
+    var prizeRef = myDataRef.child("prizes")
+    $('#addPrize').click(function() {
+        //recieve new values
+        var company = $('#prizeCompanyNameInput').val();
+        var title = $('#prizeTitleInput').val();
+        var desc = $('#prizeDescInput').val();
+        var award = $('#prizeAwardInput').val();
+
+        //add the new mentor when fields populated
+        if(company != '' && title != '' && desc != '' && award != '') {
+            prizeRef.push({award: award, company: company, title: title, description: desc});
+            $('#prizeCompanyNameInput').val('');
+            $('#prizeTitleInput').val('');
+            $('#prizeDescInput').val('');
+            $('#prizeAwardInput').val('');
+        }
+        else
+            alert("Please fill in all of the fields.");
+    });
+    prizeRef.on('child_added', function(snapshot) {
+        //response recieved from server
+        var message = snapshot.val();
+        successfulUpdate();
+    });
+
+    //schedule additions
+    var scheduleRef = myDataRef.child("schedule")
+    $('#addEvent').click(function() {
+        //recieve new values
+        var header = $('#scheduleNameInput').val();
+        var description = $('#scheduleDescInput').val();
+        var time = $('#scheduleTimeInput').val();
+        var location = $('#scheduleLocationInput').val();
+
+        //add the new mentor if fields not empty
+        if(header != '' && description != '' && time != '' && location != '') {
+            scheduleRef.push({header: header, description: description, time: time, location: location});
+            $('#scheduleNameInput').val('');
+            $('#scheduleDescInput').val('');
+            $('#scheduleTimeInput').val('');
+            $('#scheduleLocationInput').val('');
+        }
+        else
+            alert("Please fill in all of the fields.");
+    });
+    scheduleRef.on('child_added', function(snapshot) {
+        //response recieved from server
+        var message = snapshot.val();
+        successfulUpdate();
     });
 
     $('#buildBTN').click(function() {
@@ -120,5 +192,14 @@ $(document).ready(function() {
             };
         })(f);
         reader.readAsDataURL(f);
+    }
+
+    function successfulUpdate() {
+        console.log("Application updated");
+        //add later a green fade in.
+        $('#updateField').val('Application Update Successful!');
+        $('#updateField').fadeIn(250, function() {
+            $('#updateField').fadeOut(30000);
+        });
     }
 });
